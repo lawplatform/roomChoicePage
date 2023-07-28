93beeeaca2bbc3b6dac28b24c6f1aeb25c84e2ec
daehan.lawplatform <daehan@lawplatform.kr>
2023-07-28 10:33:16 +0900

    feat : check the result page

--- src/slide/Thank.tsx
+++ src/slide/Thank.tsx
@@ -1,6 +1,8 @@
 const Thank = () => {
 	return (
-		<div> 감사합니다 :)</div>
+		<div> 감사합니다 :)
+			<a href="https://airtable.com/appfFDkFAS5nc32JC/tblmDzVjBR5ol76ce/viwFTEfuQqLRwH9L9?blocks=hide"> 확인하기 </a>
+		</div>
 	)
 }
 
