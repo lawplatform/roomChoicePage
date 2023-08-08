4873d5bfa4c933a3c8fdec8f424b004b4ce8529d
tendertree <tendertree@gmail.com>
2023-08-08 23:35:37 +0900

    fix : add margin in the gallery

--- src/components/Gallery_Button.tsx
+++ src/components/Gallery_Button.tsx
@@ -5,10 +5,10 @@ interface Gallery_ButtonProps {
 }
 const Gallery_Button = ({ name, img, show }: Gallery_ButtonProps) => {
 	return (
-		<div>
+		<div className="mx-2 ">
 			<button onClick={() => show(name)}>
-				<img className="object-cover  h-20 w-44 sm:w-52 rounded-lg" src={img} alt="" />
-				<div className=" flex px-5 py-2 bg-black  text-white relative  bottom-4 sm:bottom-2">{name}</div>
+				<img className="object-cover  h-20 w-36 sm:w-52 rounded-lg" src={img} alt="" />
+				<div className=" flex px-5 w-36 py-2 bg-black  text-white relative  bottom-4 sm:bottom-2">{name}</div>
 			</button>
 		</div>
 	)
